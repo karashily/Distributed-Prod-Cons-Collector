50fd638962045d5acd016eb4c68f17b61b3c6cdf
AhmedEssamDakrory <ae00498@gmail.com>
2020-02-21 19:48:45 +0200

    editting bash file

--- machine1/Main.sh
+++ machine1/Main.sh
@@ -1,16 +1,13 @@
 
-
 python Producer.py &
 
 for i in $(seq 1 "$1");
 do
-echo "cons"
 python Consumer_otsu.py &
 done
-#for i in $(seq 1 2);
-#o
-#echo "coll"
-#python Collector1.py
-#done
+for i in $(seq 1 "$1");
+do
+python Collector1.py
+done
 
 
