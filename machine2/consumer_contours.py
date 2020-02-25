e12cff91af24625df924f347bcbfc8aecf148e8b
karashily <ahmed.elkarashily@gmail.com>
2020-02-25 22:58:38 +0200

    done el done

--- machine2/consumer_contours.py
+++ machine2/consumer_contours.py
@@ -1,15 +1,15 @@
 import zmq
 import cv2
 
-def consumer_contours(recv_port, send_port, consumer_id):
+def consumer_contours(recv_ip, recv_port, send_port, consumer_id):
     context = zmq.Context()
 
     recv_socket = context.socket(zmq.PULL)
-    recv_socket.connect("tcp://127.0.0.1:"+str(recv_port))
-
+    recv_socket.connect("tcp://{}:{}".format(recv_ip, recv_port))
+    print("contours consumer recv connected")
     send_socket = context.socket(zmq.PUSH)
     send_socket.connect("tcp://127.0.0.1:"+str(send_port))
-
+    print("contours consumer send connected")
     while True:
         # receiving
         recv_data = recv_socket.recv_pyobj()
@@ -24,5 +24,5 @@ def consumer_contours(recv_port, send_port, consumer_id):
         contours = cv2.findContours(img, mode=cv2.RETR_LIST, method=cv2.CHAIN_APPROX_SIMPLE)
 
         # sending
-        send_data = {'frame_number': frame_num, 'contours': contours}
+        send_data = {'frame_number': frame_num, 'img': contours}
         send_socket.send_pyobj(send_data)
--- machine2/machine2.py
+++ machine2/machine2.py
@@ -6,12 +6,19 @@ import sys
 
 # read n from conf file
 
-n = int(sys.argv[1])
-recv_init_port = 49162
+conf = open("machine2.conf", "r").read().split('\n')
 
+n = int(conf[1])
+machine1_ip = conf[3]
+
+print(n, machine1_ip)
+
+# n = int(sys.argv[1])
+recv_init_port = 49162
+"192.168.43.223"
 i = 0
 for j in range(n):
-    cons_cont = threading.Thread(target=consumer_contours, args=[recv_init_port+i, 1314, j+1])
+    cons_cont = threading.Thread(target=consumer_contours, args=[machine1_ip, recv_init_port+i, 1314, j+1])
     cons_cont.start()
     if(j%2==1):
         i += 1
